1add3176119b255debd834e7c80223902ac98659
ElectricCrash <electriccrash25@gmail.com>
2016-05-23 18:50:40 -0400

    Update AvatarCycle.php

--- AvatarCycle.php
+++ AvatarCycle.php
@@ -4,16 +4,16 @@ header('Content-Type: text/html; charset=utf-8');
 //  Twitter Setup
 // Create app and tokens at https://apps.twitter.com/
 $auth = array(
-	"consumer_key" => "-----",
-	"consumer_secret" => "-----",
-	"user_token" => "-----",
-	"user_secret" => "-----");
+	"consumer_key" => "RGOfgtsaZFB2bwxxhMTWQFMU1",
+	"consumer_secret" => "4F8RCtnxqFhDB66bvCkw2u7LQknOuZ4CHmWyTQtvEVSLFE5CFy",
+	"user_token" => "2544959155-jgzKZMwASGjjlO1QZdJDYdea4ZjEuhEEucjUNBq",
+	"user_secret" => "bq5j31H7s46NlW8iCeGLLzZCchKqm1Oko6Nqar47TxCbi");
 
 require_once "twitter.class.php";
 $twitter = new Twitter($auth["consumer_key"], $auth["consumer_secret"], $auth["user_token"], $auth["user_secret"]);
 
 // Absolute path to avatar folder
-$dir    = '/home/user/TwitterAvatars';
+$dir    = '/home/user/TwittAvatars';
 $avatarList = array_diff(scandir($dir), array('..', '.'));
 
 $select = array_rand($avatarList);
